c5d99b3429364e4565f57790d3c519bf0b77b742
Aadish Jain <121042282+mapcrafter2048@users.noreply.github.com>
2025-10-04 23:54:22 +0530

    Add source files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ChessBoard from './components/ChessBoard'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <ChessBoard />
+    </div>
+  )
+}
+
+export default App
